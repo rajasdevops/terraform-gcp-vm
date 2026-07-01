26ee659e05930dc2e83a901be602da2387bc2f55
rajasdevops <rajasdevops@gmail.com>
2026-07-01 18:34:08 +0530

    Add Google Compute Firewall for HTTP traffic

--- firewall.tf
+++ firewall.tf
@@ -1,1 +1,15 @@
+resource 
+"google_compute_firewall"
+"allow_http" {
+  name    = "allow-http"
+  network = "default"
 
+allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+source_ranges = ["0.0.0.0/0"]
+
+target_tags = ["http-server"]
+}
