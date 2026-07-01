d41aa90944a474d7f5bb240a375fabface67b0ef
rajasdevops <rajasdevops@gmail.com>
2026-07-01 18:48:49 +0530

    Update firewall.tf

--- firewall.tf
+++ firewall.tf
@@ -1,14 +1,16 @@
-resource 
-"google_compute_firewall"
-"allow_http" {
+resource "google_compute_firewall" "allow_http" {
   name    = "allow-http"
   network = "default"
+
 
 allow {
     protocol = "tcp"
     ports    = ["80"]
   }
-  source_ranges = ["0.0.0.0/0"]
 
-  target_tags = ["http-server"]
+
+source_ranges = ["0.0.0.0/0"]
+
+
+target_tags = ["http-server"]
 }
